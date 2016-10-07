bf1a76901b5a960ffa6bba8f277839ef746849a7
packetdog2000 <edwingarcia@gmail.com>
2016-10-07 14:00:05 -0500

    first commit

---
+++ webpack.config.js
@@ -0,0 +1,33 @@
+module.exports = {
+    context: __dirname+'/src',
+    entry: [
+      './index.js'
+    ],
+    output: {
+      path: __dirname,
+      publicPath: '/',
+      filename: 'bundle.js'
+    },
+    module: {
+      loaders: [{
+        exclude: /node_modules/,
+        loader: 'babel',
+        query: {
+          presets: ['react', 'es2015', 'stage-1']
+        }
+    },
+    { test: /\.(woff|woff2)$/,  loader: "url-loader?limit=10000&mimetype=application/font-woff" },
+         { test: /\.ttf$/,    loader: "file-loader" },
+         { test: /\.eot$/,    loader: "file-loader" },
+         { test: /\.svg$/,    loader: "file-loader" }
+     ]
+    },
+    resolve: {
+      extensions: ['', '.js', '.jsx']
+    },
+    devServer: {
+        inline: true,
+      historyApiFallback: true,
+      contentBase: './'
+    }
+  };
